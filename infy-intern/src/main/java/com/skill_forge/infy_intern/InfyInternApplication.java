ab7200d307adce70613e72c914a854b0e7b91e0d
rakesh <rmahapatra471@gmail.com>
2025-10-19 23:10:58 +0530

    Initial commit

---
+++ infy-intern/src/main/java/com/skill_forge/infy_intern/InfyInternApplication.java
@@ -0,0 +1,13 @@
+package com.skill_forge.infy_intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyInternApplication.class, args);
+	}
+
+}
---
+++ infy-intern/src/test/java/com/skill_forge/infy_intern/InfyInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill_forge.infy_intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
